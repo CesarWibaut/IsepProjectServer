af7cc123169a6f15162096b6cadba23b1c39e220
César Wibaut <cesar.wibaut@gmail.com>
2018-04-15 16:24:07 +0200

    Register WIP 2

---
+++ src/main/java/servlet/Register.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import static api.BDDFactory.getDbi;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.NotFoundException;
+
+import dao.UsersDAO;
+
+/**
+ * Servlet implementation class Register
+ */
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static UsersDAO usersDao = getDbi().open(UsersDAO.class);
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String first = request.getParameter("first");
+		String last = request.getParameter("last");
+		String email = request.getParameter("email");
+		String password = request.getParameter("pwd");
+		usersDao.insert(first, last, email, password);
+		response.sendRedirect("Test");
+	}
+
+}
