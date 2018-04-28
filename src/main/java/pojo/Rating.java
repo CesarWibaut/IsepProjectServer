0389454e83190e12e588768738fb7d82f9432afa
César Wibaut <cesar.wibaut@gmail.com>
2018-04-28 18:39:46 +0200

    The Rating POJO

---
+++ src/main/java/pojo/Rating.java
@@ -0,0 +1,34 @@
+package pojo;
+
+public class Rating {
+	String fid;
+	String email;
+	String rid;
+	String score;
+	public String getFid() {
+		return fid;
+	}
+	public void setFid(String fid) {
+		this.fid = fid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRid() {
+		return rid;
+	}
+	public void setRid(String rid) {
+		this.rid = rid;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	
+	
+}
